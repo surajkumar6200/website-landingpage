7c9e74fd6e3b8fd5ca00795ec5616ffb3c3b92c9
surajkumar6200 <102767042+surajkumar6200@users.noreply.github.com>
2025-02-10 07:04:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    
+});
+
+  
